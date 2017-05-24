813fdbdbb3355b3a19ebb4f3db3af3e3610015ae
rsperoni <rsperoni@mgcoders.com>
2017-05-24 14:01:26 -0300

    Material

--- src/client/app/home/home.module.ts
+++ src/client/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { NameListService } from '../shared/name-list/name-list.service';
 import { ToolService } from '../_services/tool.service';
+import { MdListModule } from '@angular/material';
 
 @NgModule({
-  imports: [HomeRoutingModule, SharedModule],
+  imports: [HomeRoutingModule, SharedModule.forRoot(),MdListModule],
   declarations: [HomeComponent],
   exports: [HomeComponent],
   providers: [NameListService, ToolService]
