545cf53dc38e701945c300e20b0b3eae07812d8d
rsperoni <rsperoni@mgcoders.com>
2017-05-26 08:29:40 -0300

    aws delete porquerias

--- stop_server.sh
+++ stop_server.sh
@@ -5,3 +5,7 @@ echo cd tt-bot-deploy en home
 cd /home/ubuntu/omicflows-frontend-deploy
 echo docker-compose kill
 docker-compose -f docker-compose.production.yml kill
+# Delete all stopped containers
+docker ps -q -f status=exited | xargs --no-run-if-empty docker rm
+# Delete all dangling (unused) images
+docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi
