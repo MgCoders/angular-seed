c8817027ca8eaa440e3e9d5864ab9636c01edb6f
rsperoni <rsperoni@mgcoders.com>
2017-05-26 07:59:52 -0300

    cambio compose prod

--- start_server.sh
+++ start_server.sh
@@ -5,4 +5,4 @@ $(aws ecr get-login --region $AWS_DEFAULT_REGION)
 echo cd omicflows-frontend-deploy en home
 cd /home/ubuntu/omicflows-frontend-deploy
 echo docker-compose up
-docker-compose -f docker-compose.production.yml build && docker-compose -f docker-compose.production.yml up angular-seed && docker-compose -f docker-compose.production.yml up -d angular-seed-nginx
+docker-compose -f docker-compose.production.yml build && docker-compose -f docker-compose.production.yml pull && docker-compose -f docker-compose.production.yml up angular-seed && docker-compose -f docker-compose.production.yml up -d angular-seed-nginx
